70cf33b0cee1d26c596bc3ca1be2f72ad5326aff
mahesh2225 <g.mahendra22@gmail.com>
2019-12-22 16:02:00 +0530

    created modal for create post

---
+++ js/index.js
@@ -0,0 +1,10 @@
+function createPost(){
+    SignUp.style.display = "none";
+    SignIn.style.display = "none";
+    CreatePost.style.display = "block";
+}
+function close_CreatePost(){
+
+    CreatePost.style.display = "none";
+
+}
