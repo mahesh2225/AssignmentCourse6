1e5cb7031dec7ad577b34104f625c9bd7d2bbc99
mahesh2225 <g.mahendra22@gmail.com>
2019-12-22 15:32:43 +0530

    Two modals created for signup and signin respectively

---
+++ js/common.js
@@ -0,0 +1,19 @@
+function signUp(){
+    let signInEl = document.getElementById('SignIn')
+    signInEl.style.display = "none";
+    let signUpEl = document.getElementById('SignUp')
+    signUpEl.style.display = "block";
+}
+function close_signUp(){
+    let signUpEl = document.getElementById('SignUp')
+    signUpEl.style.display = "none";
+}
+function signIn(){
+    let signInEl = document.getElementById('SignIn')
+    signInEl.style.display = "block";
+}
+
+function close_signIn(){
+    let signInEl = document.getElementById('SignIn')
+    signInEl.style.display = "none";
+}
